b4ebc11f32dae49a54839bc76bea52f83a0d0943
RafaelSilva27 <radasilva789@gmail.com>
2023-10-18 17:09:04 +0200

    commit

--- docker/www/relacion3.3/3_3_4.php
+++ docker/www/relacion3.3/3_3_4.php
@@ -6,35 +6,11 @@
   <title>Document</title>
 </head>
 <body>
-  <?php
-    $array = [];
-
-    for ($i = 0; $i < 100; $i++){
-      $array[$i] = rand(0, 20);
-      echo $array[$i] .  " ";
-    }
-  ?>
-  <form  method="post">
-    <br>Introduce el primer valor <input type="number" name="num1">
-    <br>Introduce el segundo valor <input type="number" name="num2">
-    <input type="submit" value="Cambiar">
-  </form> 
-  
-  <?php
-    if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["num1"]) && isset($_POST["num2"])) {
-      $num1 = intval($_POST["num1"]);
-      $num2 = intval($_POST["num2"]);
-
-      for ($i = 0; $i < count($array); $i++) {
-        if ($array[$i] === $num1) {
-            $array[$i] = $num2;
-        }
-    }
-
-    echo "<br>Números modificados: " . implode(' ', $array);
-
-
-    }
-  ?>
-</body>
+  <p>Introduce dos numeros y el primero te lo cambiare por el segundo</p>
+  <form method="post" action="3_3_4b.php">
+    Numero 1<input type="number" name="num1"><br>
+    Numero 2<input type="number" name="num2">
+    <input type="submit" name="Enviar"> 
+  </form>
+</body> 
 </html>
--- docker/www/relacion3.3/3_3_6.php
+++ docker/www/relacion3.3/3_3_6.php
@@ -6,6 +6,32 @@
   <title>Document</title>
 </head>
 <body>
-  
+  <?
+    $diccionario = [
+      "coche" => "car",
+      "avion" => "plane",
+      "camion" => "truck",
+      "moto" => "bike",
+      "manzana" => "apple",
+      "pera" => "pear",
+      "ventana" => "window",
+      "puerta" => "door",
+      "colegio" => "school",
+      "gimnasio" => "gym",
+      "bomba" => "book",
+      "boligrafo" => "pencil",
+      "fiesta" => "party",
+      "arbol" => "tree",
+      "roca" => "rock",
+      "oro" => "gold",
+      "plata" => "silver",
+      "cobre" => "copper",
+      "bicho" => "bug",
+      "fuego" => "fire"
+    ];
+    
+    echo "<h1>Diccionario</h1>"
+    
+  ?>
 </body>
 </html>
